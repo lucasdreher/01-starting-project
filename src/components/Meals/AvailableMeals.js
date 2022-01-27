cfd9dc92b9a4764426e302f4c05f9bb42bc53f92
Lucas Dreher <lucasdreher@gmail.com>
2022-01-27 08:26:12 +0100

    meals are fetch from server

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,44 +1,66 @@
+import { useEffect, useState } from 'react';
+
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
 import MealItem from './MealItem/MealItem';
-
-const DUMMY_MEALS = [
-	{
-		id: 'm1',
-		name: 'Sushi',
-		description: 'Finest fish and veggies',
-		price: 22.99
-	},
-	{
-		id: 'm2',
-		name: 'Schnitzel',
-		description: 'A german specialty!',
-		price: 16.5
-	},
-	{
-		id: 'm3',
-		name: 'Barbecue Burger',
-		description: 'American, raw, meaty',
-		price: 12.99
-	},
-	{
-		id: 'm4',
-		name: 'Green Bowl',
-		description: 'Healthy...and green...',
-		price: 18.99
-	}
-];
+import useHttp from '../../hooks/use-http';
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => (
-		<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
-	));
+	const [ meals, setMeals ] = useState([]);
+
+	const { isLoading, error, sendRequest: fetchMeals } = useHttp();
+
+	useEffect(
+		() => {
+			const transformMeals = (mealObj) => {
+				const loadedMeals = [];
+				for (const mealKey in mealObj) {
+					loadedMeals.push({
+						id: mealKey,
+						name: mealObj[mealKey].name,
+						description: mealObj[mealKey].description,
+						price: mealObj[mealKey].price
+					});
+				}
+
+				setMeals(loadedMeals);
+			};
+
+			fetchMeals(
+				{ url: 'https://react-http-88f71-default-rtdb.europe-west1.firebasedatabase.app/meals.json' },
+				transformMeals
+			);
+		},
+		[ fetchMeals ]
+	);
+
+	let mealsList = <h2>No meals found.</h2>;
+
+	if (meals.length > 0) {
+		mealsList = (
+			<ul>
+				{meals.map((meal) => (
+					<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />
+				))}
+			</ul>
+		);
+	}
+
+	let content = mealsList;
+
+	if (error) {
+		content = <button onClick={fetchMeals}>Try again</button>;
+	}
+
+	if (isLoading) {
+		content = 'Loading tasks...';
+	}
 
 	return (
 		<section className={classes.meals}>
-			<Card>
-				<ul>{mealsList}</ul>
-			</Card>
+			<div className={classes.meals}>
+				<Card>{content}</Card>
+			</div>
 		</section>
 	);
 };
